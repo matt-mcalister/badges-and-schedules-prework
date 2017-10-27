3aa9cdfa0664f7141f3a950421c6424719d10cf9
Matt McAlister <matt.mcalister93@gmail.com>
2017-10-27 02:29:10 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  result = []
+  attendees.each do |name| 
+    result << badge_maker(name)
+  end
+  result
+end
+
+def assign_rooms(attendees)
+  result = []
+  attendees.each_with_index do |name,idx|
+    room = idx + 1
+    result << "Hello, #{name}! You'll be assigned to room #{room}!"
+  end
+result
+end
+
+def printer(attendees)
+  puts batch_badge_creator(attendees)
+  puts assign_rooms(attendees)
+end
